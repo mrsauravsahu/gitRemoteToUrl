fe888b38c667df5d6d3f5d8f9b09314cc97c2c13
Saurav Sahu <sauravsahu.12@live.com>
2018-01-26 20:54:58 +0530

    add yarn.lock file. update .gitignore. refactor gitRemoteToUrl.js
    add yarn.lock file. update .gitignore. refactor gitRemoteToUrl.js

--- gitRemoteToUrl.js
+++ gitRemoteToUrl.js
@@ -1,29 +1,33 @@
 const gitRemoteToUrl = (remote) => {
-  const HOSTCOLONREGEX = /@[a-zA-Z0-9\.]+:/;
-  const HOSTREPOREGEX = /[a-zA-Z0-9]+@[a-zA-Z0-9\.]+/;
+  if (typeof remote !== 'string' || remote === '') return '';
 
+  const hostRegex = /@[a-zA-Z0-9\.]+:/;
+  const hostRepositoryRegex = /[a-zA-Z0-9]+@[a-zA-Z0-9\.]+/;
+  const noPrefixRegex = /^[a-zA-Z]+:\/\//;
   const http = 'http';
-  let gitRemote = remote;
 
-  if (typeof gitRemote !== 'string' || gitRemote === '') {
-    return '';
-  } else if (gitRemote.startsWith(http)) return gitRemote;
-  else if (gitRemote.startsWith('ssh')) {
-    // Replace 'ssh://' with 'http://'
-    gitRemote = gitRemote.replace(/^ssh/, http);
-  } else {
-    gitRemote = `${http}://${gitRemote}`;
+  // Copy remote string
+  let gitRemote = remote.substr();
+
+  if (gitRemote.startsWith(http)) {
+    return gitRemote;
   }
-  // Remove : after host and repo
-  const hostRepoMatches = gitRemote.match(HOSTCOLONREGEX) || [];
-  if (hostRepoMatches.length > 0) {
-    gitRemote = gitRemote.replace(HOSTCOLONREGEX, p => `${p.substr(0, p.length - 1)}/`);
+
+  // Replace 'ssh' prefix  with 'http'
+  gitRemote = gitRemote.replace(/^ssh/, http);
+
+  if (!gitRemote.match(noPrefixRegex)) {
+    gitRemote = `http://${gitRemote}`;
+  }
+
+  // Remove ':' after host and repository
+  if (gitRemote.match(hostRegex)) {
+    gitRemote = gitRemote.replace(hostRegex, p => `${p.substr(0, p.length - 1)}/`);
   }
 
   // Remove user from user@host format for ssh
-  const userAtHostMatches = gitRemote.match(HOSTREPOREGEX) || [];
-  if (userAtHostMatches.length > 0) {
-    gitRemote = gitRemote.replace(HOSTREPOREGEX, p => p.split('@')[1]);
+  if (gitRemote.match(hostRepositoryRegex)) {
+    gitRemote = gitRemote.replace(hostRepositoryRegex, p => p.split('@')[1]);
   }
   return gitRemote;
 };
